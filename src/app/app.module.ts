6cfd28a5282b3a6b6d695d39085fde551a28fa31
thiagolboliveira1 <thiagolboliveira@hotmail.com>
2024-07-31 08:28:33 -0300

    Projeto finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CalculatorComponent } from './calculator/calculator.component';
+import { CalculoComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculatorComponent
+    CalculoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-calculator',
+  selector: 'app-calculo',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.css']
 })
-export class CalculatorComponent implements OnInit {
+export class CalculoComponent {
+  valorTotal: number = 0;
+  percentualComissao: number = 0;
+  comissao: number | null = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  calcularComissao() {
+    if (this.valorTotal > 0 && this.percentualComissao > 0) {
+      this.comissao = (this.valorTotal * this.percentualComissao) / 100;
+    } else {
+      this.comissao = null;
+    }
   }
 
+  formatarNumero(valor: number | null): string {
+    if (valor === null) {
+      return '';
+    }
+
+    let formatarValor = valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    if (valor > 1000){
+      const partes = formatarValor.split(',');
+      if (partes.length > 1 && partes[1].length === 2) {
+        formatarValor = partes[0] + ',' + partes[1] + '0';
+      }
+    }
+    return formatarValor;
+  }
+
+
+
+  formatarPercentual(valor: number): string {
+    return valor.toFixed(2).replace('.', ',') + '%';
+  }
 }
